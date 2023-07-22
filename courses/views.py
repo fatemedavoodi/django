5716c5ebe5a474578984318ba5c7f9de34e9fd8a
fatemehd97 <bahare.khazan98@gmail.com>
2023-07-22 12:22:42 -0700

    create course model

---
+++ courses/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from .models import Course
+
+
+def courses(request):
+    course = Course.objects.all()
+    context = {
+        'courses':course
+    }
+    return render(request,'course/courses.html',context=context)
